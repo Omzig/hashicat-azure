92fc5810a0fa1b714d59077dd37d6a2e6c8a5672
Omzig <mmorrow@rocketmail.com>
2020-01-08 21:24:17 +0000

    Updated website.

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
 
+tags = {
+  environment = "production"
+}
+
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
